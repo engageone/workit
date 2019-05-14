dbedca0bee5af93366563dde335ede0da24d63a8
engageone <42962238+engageone@users.noreply.github.com>
2019-05-14 18:45:47 +0530

    side-buttons

---
+++ js-side/en.js
@@ -0,0 +1,27 @@
+"CtaButtonSettings": {
+        "button1": {
+            "card": "d2166039",
+            "label": "Contact",
+            "icon": "contact"
+        },
+        "button2": {
+            "url": "https://www.cimb.com",
+            "label": "Website",
+            "icon": "website"
+        },
+        "button3": {
+            "url": "mailto:callcentre@cimb.com?Subject=EngageOne%23Video%20Support%20Request",
+            "label": "Email",
+            "icon": "email"
+        },
+        "button4": {
+            "card": "acddb957",
+            "label": "Save",
+            "icon": "save"
+        },
+        "button5": {
+            "card": "9ac5a578",
+            "label": "Share",
+            "icon": "share"
+        }
+    },
