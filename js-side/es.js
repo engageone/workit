a0c5b379445f34cc2be894c25ee87f9366bcc984
engageone <42962238+engageone@users.noreply.github.com>
2019-05-14 18:46:37 +0530

    side button

---
+++ js-side/es.js
@@ -0,0 +1,27 @@
+"CtaButtonSettings": {
+        "button1": {
+            "card": "7b480c70",
+            "label": "Botón 1",
+            "icon": "contact"
+        },
+        "button2": {
+            "card": "7b480c70",
+            "label": "Botón 2",
+            "icon": "website"
+        },
+        "button3": {
+            "card": "7b480c70",
+            "label": "Botón 3",
+            "icon": "email"
+        },
+        "button4": {
+            "card": "7b480c70",
+            "label": "Botón 4",
+            "icon": "save"
+        },
+        "button5": {
+            "url": "http://example.com/es",
+            "label": "Botón 5",
+            "icon": "share"
+        }
+    },
