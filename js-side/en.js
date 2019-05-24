f592ff25e9bff6bcfef87dcc1b64fb85180c7228
engageone <42962238+engageone@users.noreply.github.com>
2019-05-24 16:08:51 +0530

    Update en.js
    saved

--- js-side/en.js
+++ js-side/en.js
@@ -25,3 +25,94 @@
             
         }
     },
+"ChapterSettings": [
+        {
+            "label": "Introduction",
+            "states": [
+                {
+                    "cardId": "START",
+                    "width": 10
+                }
+            ]
+        },
+        {
+            "label": " Needs",
+            "states": [
+                {
+                    "cardId": " fe1820ca",
+                    "width": 10
+                },
+                
+            ]
+        },
+        {
+            "label": "EstateLiquidity",
+            "states": [
+                {
+                    "cardId": "a6003b49",
+                    "width": 10
+                },
+            ]
+        },
+        {
+            "label": " portifolio diversification",
+            "states": [
+                {
+                    "cardId": "f3a7ae85",
+                    "width": 10
+                },
+                
+            ]
+        },
+        {
+            "label": " Business Continuity",
+            "states": [
+                {
+                    "cardId": "247089d6",
+                    "width": 10
+                },
+                
+            ]
+        },
+        
+        {
+            "label": " Final_Needs",
+            "states": [
+                {
+                    "cardId": "4d18f00e",
+                    "width": 10
+                },
+                
+            ]
+        }, 
+         {
+            "label": " Customer Experiance",
+            "states": [
+                {
+                    "cardId": "c0024b69",
+                    "width": 10
+                },
+                
+            ]
+        },
+                 {
+            "label": " Speak to Rm",
+            "states": [
+                {
+                    "cardId": "27f7ae70",
+                    "width": 10
+                },
+                
+            ]
+        }, 
+         {
+            "label": " why donot like to speak to RM",
+            "states": [
+                {
+                    "cardId": "dad66a40",
+                    "width": 20
+                },
+                
+            ]
+        }, 
+    ]
